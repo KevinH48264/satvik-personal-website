fbf94ac648d45af560be4203e427d21edfbac296
KevinH48264 <1kevin.huang@gmail.com>
2020-08-22 12:35:06 -0400

    Updates

---
+++ precache-manifest.0f27646be6a879b5ccd21424ac4bdd1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c732faab60b497e6be386476ffbf066",
+    "url": "/satvik-personal-website/index.html"
+  },
+  {
+    "revision": "64b844847a8b98ad5df7",
+    "url": "/satvik-personal-website/static/css/main.f52ea196.chunk.css"
+  },
+  {
+    "revision": "5992e1702b813d96206f",
+    "url": "/satvik-personal-website/static/js/2.f7215e98.chunk.js"
+  },
+  {
+    "revision": "da42135372d3365a7233548ad1382c4d",
+    "url": "/satvik-personal-website/static/js/2.f7215e98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64b844847a8b98ad5df7",
+    "url": "/satvik-personal-website/static/js/main.ea4bcd57.chunk.js"
+  },
+  {
+    "revision": "8ad8d5bbe461bc7c8772",
+    "url": "/satvik-personal-website/static/js/runtime-main.4032f741.js"
+  }
+]);
