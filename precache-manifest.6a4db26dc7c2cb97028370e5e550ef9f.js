0dd364c3f5e5c8cb53081c66a4b2b4d12a5919cf
KevinH48264 <1kevin.huang@gmail.com>
2020-08-22 12:29:27 -0400

    Updates

---
+++ precache-manifest.6a4db26dc7c2cb97028370e5e550ef9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4418b440b6f9a1be5a7a1b81bce8f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64b844847a8b98ad5df7",
+    "url": "/static/css/main.f52ea196.chunk.css"
+  },
+  {
+    "revision": "40c7728a2d752312eaa8",
+    "url": "/static/js/2.fde8e311.chunk.js"
+  },
+  {
+    "revision": "da42135372d3365a7233548ad1382c4d",
+    "url": "/static/js/2.fde8e311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64b844847a8b98ad5df7",
+    "url": "/static/js/main.ea4bcd57.chunk.js"
+  },
+  {
+    "revision": "681a71b94d6c9476ba75",
+    "url": "/static/js/runtime-main.2e486dfb.js"
+  }
+]);
